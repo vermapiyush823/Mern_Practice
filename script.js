81b4b976f616c9987d265fcca0ac4a2eb695eae6
vermapiyush823 <vermapiyush823@gmail.com>
2023-11-09 00:53:14 +0530

    commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// let func1 =  function (){
+//     console.log("Hello World!");
+// };
+// func1(); // func
+
+// Arrow function
+
+let func1 = () =>{
+    console.log("Hello World");
+};
+func1();
